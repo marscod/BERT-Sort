176a3f7c453ab335f6be787eb9b88810a59b353a
Mehdi <mbahrami@ucmerced.edu>
2022-04-30 12:55:37 -0700

    Update run_flaml_docker.sh

--- run_flaml_docker.sh
+++ run_flaml_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -v /home/xp/repo/fla/BERT-S/BERT-Sort:/BERT-Sort -it -w /BERT-Sort/automl/flaml --entrypoint python3 automl flaml_re.py
+sudo docker run --rm -v $(pwd):/BERT-Sort -it -w /BERT-Sort/automl/flaml --entrypoint python3 automl flaml_re.py
