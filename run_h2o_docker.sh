7730ffc9e213426bc3447a01b2c98c0eebd0e553
Mehdi <mbahrami@ucmerced.edu>
2022-04-30 12:55:56 -0700

    Update run_h2o_docker.sh

--- run_h2o_docker.sh
+++ run_h2o_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -v /home/xp/repo/fla/BERT-S/BERT-Sort:/BERT-Sort -it -w /BERT-Sort/automl/h2o --entrypoint python3 automl h2o_re.py
+sudo docker run --rm -v $(pwd):/BERT-Sort -it -w /BERT-Sort/automl/h2o --entrypoint python3 automl h2o_re.py
