0395eba20b99614109fe51e09cb7e6397e208fc5
Mehdi <mbahrami@ucmerced.edu>
2022-04-30 12:55:16 -0700

    Update run_autogluon_docker.sh

--- run_autogluon_docker.sh
+++ run_autogluon_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -v /home/xp/repo/fla/BERT-S/BERT-Sort:/BERT-Sort -it -w /BERT-Sort/automl/autogluon --entrypoint python3 automl autogluon_re.py
+sudo docker run --rm -v $(pwd):/BERT-Sort -it -w /BERT-Sort/automl/autogluon --entrypoint python3 automl autogluon_re.py
