7e0bafa1fb76d623d815214c5f17088231c15eb7
Mehdi <mbahrami@ucmerced.edu>
2022-04-30 12:56:13 -0700

    Update run_mljar_docker.sh

--- run_mljar_docker.sh
+++ run_mljar_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -v /home/xp/repo/fla/BERT-S/BERT-Sort:/BERT-Sort -it -w /BERT-Sort/automl/mljar --entrypoint python3 automl mljar_re.py
+sudo docker run --rm -v $(pwd):/BERT-Sort -it -w /BERT-Sort/automl/mljar --entrypoint python3 automl mljar_re.py
